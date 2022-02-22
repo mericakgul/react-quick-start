8f4afe274d0e7bd60c850691180585a9805e837a
mericakgul <makgul@worth.systems>
2022-02-22 14:41:05 +0100

    adding bootstrap to React

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
     "title": "CS Courses for 2018-2019",
     "courses": {
-        "F101" : {
-            "id" : "F101",
-            "meets" : "MWF 11:00-11:50",
-            "title" : "Computer Science: Concepts, Philosophy, and Connections"
+        "F101": {
+            "id": "F101",
+            "meets": "MWF 11:00-11:50",
+            "title": "Computer Science: Concepts, Philosophy, and Connections"
         },
-        "F110" : {
-            "id" : "F110",
-            "meets" : "MWF 10:00-10:50",
-            "title" : "Intro Programming for non-majors"
+        "F110": {
+            "id": "F110",
+            "meets": "MWF 10:00-10:50",
+            "title": "Intro Programming for non-majors"
         },
-        "S313" : {
-            "id" : "S313",
-            "meets" : "TuTh 15:30-16:50",
-            "title" : "Tangible Interaction Design and Learning"
+        "S313": {
+            "id": "S313",
+            "meets": "TuTh 15:30-16:50",
+            "title": "Tangible Interaction Design and Learning"
         },
-        "S314" : {
-            "id" : "S314",
-            "meets" : "TuTh 9:30-10:50",
-            "title" : "Tech & Human Interaction"
+        "S314": {
+            "id": "S314",
+            "meets": "TuTh 9:30-10:50",
+            "title": "Tech & Human Interaction"
         }
     }
 };
 
-const App = () =>  (
-    <div>
-        <Banner title={ schedule.title } />
-        <CourseList courses={ schedule.courses } />
+const App = () => (
+    <div className="container">
+        <Banner title={schedule.title}/>
+        <CourseList courses={schedule.courses}/>
     </div>
 );
 
-const Banner = ({ title }) => (
-    <h1>{ title }</h1>
+const Banner = ({title}) => (
+    <h1>{title}</h1>
 );
 
-const CourseList = ({ courses }) => (
-    <div>
-        { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+const CourseList = ({courses}) => (
+    <div className="course-list">
+        {Object.values(courses).map(course => <Course key={course.id} course={course}/>)}
     </div>
 );
 
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+const terms = {F: 'Fall', W: 'Winter', S: 'Spring'};
 
 const getCourseTerm = course => (
     terms[course.id.charAt(0)]
@@ -53,10 +54,14 @@ const getCourseNumber = course => (
     course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
-    <div>
-        { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+const Course = ({course}) => (
+    <div className="card m-1 p-2">
+        <div className="card-body">
+            <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
+            <div className="card-text">{course.title}</div>
+        </div>
     </div>
 );
+
 
 export default App;
